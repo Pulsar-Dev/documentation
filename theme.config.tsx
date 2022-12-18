183f03204c9f0a20aa2702cfbd8b04bf32ecabd0
Lythium <62567544+Lythium4848@users.noreply.github.com>
2022-12-18 15:53:10 +0000

    theme changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Pulsar Docs</span>,
+  logo: (
+    <>
+      <img src="https://raw.githubusercontent.com/Pulsar-Dev/documentation/main/public/pulsar.png" alt="logo" width="50" height="50"/>
+      <span style={{ marginLeft: '.4em', fontWeight: 500 }}>
+        Pulsar Docs
+      </span>
+    </>
+  ),
+  logoLink: "https://raw.githubusercontent.com/Pulsar-Dev/documentation/main/public/pulsar.png",
   project: {
     link: 'https://github.com/Pulsar-Dev/documentation',
   },
@@ -15,7 +25,26 @@ const config: DocsThemeConfig = {
   },
   sidebar: {
     defaultMenuCollapseLevel: 0,
-  }
+  },
+  head: () => {
+    const { asPath } = useRouter()
+    const { frontMatter } = useConfig()
+    return <>
+      <meta property="og:url" content={`https://docs.lythium.vip${asPath}`} />
+      <meta property="og:title" content={frontMatter.title || 'Pulsar Docs'} />
+      <meta property="og:description" content={frontMatter.description} />
+    </>
+  },
+  useNextSeoProps() {
+    const { route } = useRouter()
+    if (route !== '/') {
+      return {
+        titleTemplate: '%s – Pulsar Docs'
+      }
+    }
+  },
+  primaryHue: 190,
+
 }
 
 export default config
