a69041579ac8c34c2b3ce76ea5a41669a1a75e62
Lythium4848 <62567544+Lythium4848@users.noreply.github.com>
2023-06-29 15:49:38 +0100

    🦍

--- components/method/argument.jsx
+++ components/method/argument.jsx
@@ -3,20 +3,20 @@ import Link from "next/link";
 export default function Argument({ type, name, defaultValue, children }) {
 	return (
 		<>
-			<h3 class="nx-font-semibold nx-tracking-tight nx-text-slate-900 dark:nx-text-slate-100 nx-mt-6">
+			<h3 className="nx-font-semibold nx-tracking-tight nx-text-slate-900 dark:nx-text-slate-100 nx-mt-6">
 				<Link className="nx-text-primary-600 nx-decoration-from-font [text-underline-position:from-font]" href={`https://wiki.facepunch.com/gmod/${type}`} target="_blank">
 					{type}
 				</Link>
 				{` ${name}`}
 				{
 					defaultValue && (
-						<span class="nx-text-gray-500 dark:nx-text-gray-400">
+						<span className="nx-text-gray-500 dark:nx-text-gray-400">
 							{` = ${defaultValue}`}
 						</span>
 					)
 				}
 			</h3>
-			<p class="nx-leading-7 first:nx-mt-0">
+			<p className="nx-leading-7 first:nx-mt-0">
 				{children}
 			</p>
 		</>
